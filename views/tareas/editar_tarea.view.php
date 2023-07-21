19bd348fa3eff7914716068f574091ab1852f391
xaviphp <33201295+xaviphp@users.noreply.github.com>
2023-07-21 18:14:12 +0200

    commit

--- views/tareas/editar_tarea.view.php
+++ views/tareas/editar_tarea.view.php
@@ -20,10 +20,6 @@
           <input type="text" id="descripcion" name="descripción">
         </div>
         <div class="form-group">
-          <label for="fecha_creacion">Fecha de Creación</label>
-          <input type="date" id="fecha_creacion" name="fecha_creación" required>
-        </div>
-        <div class="form-group">
           <label for="fecha_actividad">Fecha de Actividad</label>
           <input type="date" id="fecha_actividad" name="fecha_actividad" required>
         </div>
@@ -31,10 +27,8 @@
           <label for="duracion">Duración</label>
           <input type="text" id="duracion" name="duración">
         </div>
-        <div class="form-group">
-          <label for="estado">Estado</label>
-          <input type="text" id="estado" name="estado">
-        </div>
+        <label for="completada">Tarea completada:</label>
+<input type="checkbox" id="completada" name="completada" value="1" <?php echo $tarea['completada'] ? 'checked' : ''; ?>>
         <button type="submit">Editar Actividad</button>
       </form>
     </div>
