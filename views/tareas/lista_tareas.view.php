38da2e352f7ba60ab2284632dbca6d639c01f53a
xaviphp <33201295+xaviphp@users.noreply.github.com>
2023-07-21 19:29:54 +0200

    aaa

--- views/tareas/lista_tareas.view.php
+++ views/tareas/lista_tareas.view.php
@@ -14,7 +14,7 @@
 	<div class="container">
     <div class="header">
 		<h1>Do-It</h1>
-		<button class="add-button">+</button>
+        <a href="anadir_tarea.php" target=""><button class="add-button">+</button></a>
 	</div>
 	<div class="notes">
 	
@@ -22,16 +22,16 @@
 	<?php foreach ($resultadosconsulta as $resultadoconsulta): ?>
 		<div class="fecha-group">
 			<h3 class="fecha-titulo"><?php echo $resultadoconsulta['fecha_actividad']; ?></h3>
-			<?php endforeach; ?>
+			
 			<?php foreach ($resultadosconsulta as $resultadoconsulta): ?>
             <div class="nota">
                 <h3 class="nota-titulo"><?php echo $resultadoconsulta['titulo']?></h3>
                 <p class="nota-descripcion"><?php echo $resultadoconsulta['descripcion']; ?></p>
             </div>
+			<?php endforeach; ?>
     	</div>
 	<?php endforeach; ?>
 	
-        <a href="anadir_tarea.php" target="">Crear Tarea</a>
 		<a href="index.php" target="">Volver</a>
 	</div>
 </div>
